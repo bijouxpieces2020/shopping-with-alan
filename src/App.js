7714e23a6baae74b320a644650b16abf510f28ec
Toyah-Maria Batson <tmebatson@gmail.com>
2022-04-27 19:15:58 -0400

    ai button

--- src/App.js
+++ src/App.js
@@ -1,25 +1,96 @@
-import logo from './logo.svg';
-import './App.css';
+import { useState, useEffect, useRef } from "react";
+import alanBtn from "@alan-ai/alan-sdk-web";
+import "./App.css";
+
 
 function App() {
-  return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
-  );
+const [cart, setCart] = useState([]);
+const [menuItems, setMenuItems] = useState([]);
+const roolElRef = useRef(null);
+const addToCart = (menuItem) => {
+
+setCart((oldCart) => {
+
+return [...oldCart, menuItem];
+
+});
+
+};
+
+
+useEffect(() => {
+
+if (!window.alanBtnInstance) {
+
+window.alanBtnInstance = alanBtn({
+
+key: "35f6b5da65e1e9d1272a969c7eca83452e956eca572e1d8b807a3e2338fdd0dc/stage",
+
+onCommand: (commandData) => {
+
+console.log("commandData.command ", commandData.command);
+
+if (commandData.command === "getMenu") {
+
+// Call the client code that will react to the received command
+
+setMenuItems(commandData.data);
+
 }
+
+},
+
+});
+
+}
+
+console.log("a------------>");
+
+}, []);
+
+return (
+
+<div className="App">
+
+<div ref={roolElRef} id="btn"></div>{" "}
+
+{menuItems.map((menuItem) => (
+
+<li key={menuItem.name}>
+
+{" "}
+
+{menuItem.name} - $ {menuItem.price} - {menuItem.category}{" "}
+
+<button onClick={() => addToCart(menuItem)}> add to cart </button>{" "}
+
+</li>
+
+))}
+
+<div>
+
+<h2> Cart </h2>{" "}
+
+</div>
+
+{cart.map((cartItem) => (
+
+<li key={cartItem.name}>
+
+{" "}
+
+{cartItem.name} - {cartItem.price} - {cartItem.category}{" "}
+
+</li>
+
+))}{" "}
+
+</div>
+
+);
+
+}
+
 
 export default App;
